4973b5f66e24bb6c376c62d846adeec1992a816e
LAPTOP-14JTPOUB\emiel <emiel.vdd@gmail.com>
2024-04-11 17:50:53 +0200

    Een begin van de WPF gemaakt en er kunnen items toegevoegd worden

--- TODO-lijst/MainWindow.xaml.cs
+++ TODO-lijst/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.IO.Ports;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +17,42 @@ namespace TODO_lijst
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        SerialPort serialPort  = new SerialPort();
+        Opdrachten Opdrachten = new Opdrachten();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void listBox_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            List<string> opdrachtenLijst = new List<string>();
+
+            Opdrachten.Lijst = txtbxtoevoegen.Text;
+
+            
+            opdrachtenLijst.Add(Opdrachten.Toevoegen());
+
+            listBox.ItemsSource = opdrachtenLijst;
+
+            //tekstvak leeg maken
+            txtbxtoevoegen.Clear();
         }
     }
 }
---
+++ TODO-lijst/Opdrachten.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Printing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TODO_lijst
+{
+    internal class Opdrachten
+    {
+		private string lijst;
+
+		public string Lijst
+		{
+			get { return lijst; }
+			set { lijst = value.ToUpper(); }
+		}
+
+		public string Toevoegen()
+		{
+			return $"-{lijst}";
+		}
+	}
+}
